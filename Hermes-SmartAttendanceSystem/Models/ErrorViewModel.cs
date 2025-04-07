e703ca2415b9fdfecbb8fa9d6094738f98bd1574
rm-rf-SeanSalvo <salvo.284024@globalcity.sti.edu.ph>
2025-04-07 22:05:02 +0800

    Add project files.

---
+++ Hermes-SmartAttendanceSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hermes_SmartAttendanceSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
